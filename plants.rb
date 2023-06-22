199ecaa7d434e5f11798a433191433abcf8114cf
Jimmy Trulock <ttrulock23@gmail.com>
2023-06-21 18:33:58 -0600

    add elsif statment

--- plants.rb
+++ plants.rb
@@ -3,6 +3,8 @@ ounces_of_water = 10
 
 if gets_sunshine && ounces_of_water >= 10
     puts "This plant will thrive"
+elsif gets_sunshine || ounces_of_water >= 10
+    puts "this plant may survive"   
 else 
     puts "This plant will not thrive"
 end
